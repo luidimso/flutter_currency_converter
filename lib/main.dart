da89a785b24f8965149374fe5c30def972332d2f
luidimso <luidi_mso@outlook.com>
2023-08-23 09:18:51 -0300

    Adding the scaffold page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 
  Uri url = Uri.https('api.hgbrasil.com', '/finance', {'key': '06a34179'});
 
 void main() async {
-  http.Response response = await http.get(url);
-  print(response.body);
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+Future<Map> get() async {
+  Uri url = Uri.https('api.hgbrasil.com', '/finance', {'key': '06a34179'});
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text("Converter"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      )
+    );
+  }
 }
